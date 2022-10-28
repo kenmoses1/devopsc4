1c8d4dc16a19b36084a0788cdf51c1ea245efc47
kenmoses1 <kenmoses033@gmail.com>
2022-10-28 13:15:14 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # devopsc4
 # Kenmoses1
+Hello there,this is Ken Moses am the author of this This is my repo in it contains my HTML, CSS and .Js its an interesting journey learning DEVops so far
---
+++ javascript.js/index.js
@@ -0,0 +1,10 @@
+
+
+let number = [2, 3, 4 ,5 ,6]
+for(let i=0; i< number.length;i++){
+console.log(number[i]);
+}
+
+
+let = Dayoftheweek (d) = ( ( (19/4) -2*19-1) + ((5*89/4) ) + ((26*(12+1)/10)) + 09 ) %7;
+console.log("dayoftheweek")
